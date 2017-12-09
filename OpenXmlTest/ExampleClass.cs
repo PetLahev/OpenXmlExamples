4d1f9c773972955af0f2283c178dccebab18d252
PetLahev <premysl.lazecky@gmail.com>
2017-12-09 21:52:57 +0100

    Spreadsheet - how to find sheet or chart based on ID

---
+++ OpenXmlTest/ExampleClass.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Packaging;
+using openXmlSpreadsheet = DocumentFormat.OpenXml.Drawing.Spreadsheet;
+
+namespace OpenXmlTest
+{
+    public class ExampleClass
+    {
+        public delegate void ReportProgressDel(string message);
+        public event ReportProgressDel ReportProgress;
+
+        private WorkbookPart _wkb;
+        private Worksheet _wks;
+        private readonly string _filePath;
+        private readonly int _sheetId;
+        private readonly string _chartId;
+
+        public ExampleClass(string filePath, int sheetId, string chartId)
+        {
+            if (string.IsNullOrWhiteSpace(filePath) || sheetId <= 0)
+            {
+                throw new ArgumentException("Either the file path or the sheet ID was not provided");                
+            }
+
+            _filePath = filePath;
+            _sheetId = sheetId;
+            _chartId = chartId;
+            OpenFile();
+        }
+
+        /// <summary> Opens the given file via the DocumentFormat package </summary>
+        /// <returns></returns>
+        private bool OpenFile()
+        {
+            try
+            {
+                var file = SpreadsheetDocument.Open(_filePath, false);
+                _wkb = file.WorkbookPart;
+                ReportProgress?.Invoke($"File opened");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ReportProgress?.Invoke($"File couldn't be opened!\n{ex.Message}");
+                return false;
+            }            
+        }
+
+        /// <summary> Returns true if sheet with given ID exists </summary>
+        public bool SheetExists
+        {
+            get
+            {
+                return _wkb.Workbook.Descendants<Sheet>().Where(s => s.SheetId == _sheetId).FirstOrDefault() != null;
+            }
+        }
+
+        /// <summary> Returns true if chart with given ID exists </summary>
+        public bool ChartExists
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_chartId)) return false;
+                if (_wks== null)
+                {                    
+                    CheckSheetExists();
+                }
+
+                return CheckChartExists();
+            }
+        }
+
+        /// <summary> Finds the Sheet based on given sheet ID </summary>        
+        /// <returns>True if exists, otherwise false</returns>
+        private bool CheckSheetExists()
+        {
+            Sheet theSheet = _wkb.Workbook.Descendants<Sheet>().Where(s => s.SheetId ==_sheetId).FirstOrDefault();
+            if (theSheet == null)
+            {
+                ReportProgress?.Invoke("Couldn't find the worksheet");
+                return false;
+            }                
+                        
+            // Retrieve a reference to the worksheet part.
+            WorksheetPart wsPart = (WorksheetPart)(_wkb.GetPartById(theSheet.Id));
+            _wks = wsPart.Worksheet;            
+            ReportProgress?.Invoke($"Sheet found: {theSheet.Name} ({theSheet.Id})");
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>        
+        /// <returns>True if a chart exists on given sheet otherwise false </returns>
+        private bool CheckChartExists()
+        {            
+            var theDrawing = _wks.Descendants<Drawing>().FirstOrDefault();
+            if (theDrawing == null)
+            {
+                ReportProgress?.Invoke("No chart on given worksheet");
+                return false;
+            }
+
+            var drawingPart = _wks.WorksheetPart.GetPartById(theDrawing.Id);
+            openXmlSpreadsheet.WorksheetDrawing wksDrawing = (openXmlSpreadsheet.WorksheetDrawing)drawingPart.RootElement;
+
+            ReportProgress?.Invoke("Drawing part found, checking chart ID");
+            // the chartObjects are stored in object called TwoCellAnchor
+            // the Drawing part will have as many TwoCellAnchor objects as many chart objects are stored on the sheet
+            foreach (var item in wksDrawing.ChildElements)
+            {
+                openXmlSpreadsheet.TwoCellAnchor twoCellAnchor = (openXmlSpreadsheet.TwoCellAnchor)item;
+                // need to get GraphicFrame object where the Chart id is stored
+                var graphicFrame = twoCellAnchor.GetFirstChild<openXmlSpreadsheet.GraphicFrame>();
+
+                /*  
+                 *  The chart Id is stored in the frame property but the ID may not be unique if someone
+                 *  1, remove the chart e.g. with Id=1
+                 *  2, save and close the file
+                 *  3, re-open the file and insert a new chart
+                 *  4, the new chart will get the Id=1
+                 *  In that case we cannot be sure  the ID is unique
+                */
+                dynamic frameProps = graphicFrame.Descendants<openXmlSpreadsheet.NonVisualDrawingProperties>().First();
+
+                /*
+                 * Since Office 2010 a new property was added called 'creationId' which has a globally unique ID (GUID)
+                 * This is more safe but available only in 2010 and higher
+                */
+                try
+                {
+                    dynamic frameProps2010 = graphicFrame.Descendants<DocumentFormat.OpenXml.Drawing.NonVisualDrawingPropertiesExtension>().First();
+                    var creationElement = frameProps2010.FirstChild.ExtendedAttributes;
+                    string guid = creationElement[0].Value;                    
+                    if (_chartId.Equals(guid)) return true;
+                }
+                catch (Exception)
+                {                    
+                    if (_chartId.Equals((string)frameProps.Id)) return true;
+                }                
+            }
+            
+            return false;
+        }
+        
+    }
+}
---
+++ OpenXmlTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenXmlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Determines if a chart exists on given sheet.");
+            Console.WriteLine("Provide sheet ID (property name:sheetId) and chart ID (property name:graphicFrame.Id)");
+
+            Console.WriteLine("Insert Sheet Id:");
+            var sheetId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insert chart Id (guid without curly brackets):");
+            var chartId = Console.ReadLine();
+                        
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Test.xlsx");
+
+            var fileManager = new ExampleClass(filePath, sheetId, chartId);
+            fileManager.ReportProgress += FileManager_ReportProgress;
+
+            var retVal = fileManager.ChartExists;
+            Console.WriteLine($"The chart was {(retVal ? "FOUND" : "NOT FOUND")}");
+            Console.ReadLine();
+        }
+
+        private static void FileManager_ReportProgress(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
